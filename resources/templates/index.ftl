ddce18f71301128dedfc45c1f8ca6663092b3027
Alicia Pérez <pericia@gmail.com>
2019-11-06 09:40:01 -0500

    Setting up project Kbeer

---
+++ resources/templates/index.ftl
@@ -0,0 +1,10 @@
+<#-- @ftlvariable name="data" type="com.kbeer.IndexData" -->
+<html>
+    <body>
+        <ul>
+        <#list data.items as item>
+            <li>${item}</li>
+        </#list>
+        </ul>
+    </body>
+</html>
---
+++ src/Application.kt
@@ -0,0 +1,48 @@
+package com.kbeer
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import freemarker.cache.*
+import io.ktor.freemarker.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+data class IndexData(val items: List<Int>)
+
